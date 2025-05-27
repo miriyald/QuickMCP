101932726a6178af14d28616606109342dccc06f
Dileep Miriyala (Moody's) <MiriyalD@moodys.com>
2025-05-27 18:06:32 -0400

    Add Support for HTTP
    Signed-off-by: Dileep Miriyala (Moody's) <MiriyalD@moodys.com>

--- src/QuickMCP.CLI/Commands/Serve/ServerCommand.cs
+++ src/QuickMCP.CLI/Commands/Serve/ServerCommand.cs
@@ -1,13 +1,15 @@
 ﻿using System.ComponentModel;
-using QuickMCP.Abstractions;
-using QuickMCP.Builders;
-using QuickMCP.Extensions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using QuickMCP.Abstractions;
+using QuickMCP.Builders;
+using QuickMCP.Extensions;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using Vertical.SpectreLogger;
+
 
 namespace QuickMCP.CLI.Commands.Serve;
 
@@ -23,13 +25,14 @@ public class ServerCommand : AsyncCommand<ServerCommandSettings>
 
         if (!string.IsNullOrWhiteSpace(settings.ServerId))
         {
-            var localFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
-                ".quickmcp");
+            var localFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".quickmcp");
             var serverFolder = Path.Combine(localFolder, "servers", settings.ServerId);
             configFile = Path.Combine(serverFolder, "config.json");
+
             if (!File.Exists(configFile))
-                throw new FileNotFoundException(
-                    $"Server configuration file not found for {settings.ServerId} at `{configFile}`");
+            {
+                throw new FileNotFoundException($"Server configuration file not found for {settings.ServerId} at `{configFile}`");
+            }
         }
 
         if (settings.EnableLogging == true)
@@ -38,7 +41,9 @@ public class ServerCommand : AsyncCommand<ServerCommandSettings>
         IMcpServerInfoBuilder? infoBuilder = null;
 
         if (!string.IsNullOrWhiteSpace(configFile))
+        {
             infoBuilder = McpServerInfoBuilder.FromConfig(configFile);
+        }
         else
         {
             var config = settings.GetBuilderConfig();
@@ -50,52 +55,60 @@ public class ServerCommand : AsyncCommand<ServerCommandSettings>
             var loggerFactory = LoggerFactory.Create(s => s.AddSpectreConsole());
             infoBuilder.AddLogging(loggerFactory);
         }
-        else
-        {
-        }
 
         var mcpServerInfo = await infoBuilder.BuildAsync();
         if (settings.EnableLogging == true)
             AnsiConsole.MarkupLine($"[bold]Initializing MCP server...[/]");
 
-        var hostBuilder = Host.CreateApplicationBuilder();
-        var mcpBuilder = hostBuilder.Services
-            .AddMcpServer()
-            .WithQuickMCP(mcpServerInfo)
-        //.WithHttpTransport();
-        .WithStdioServerTransport();
-
-
-        // if (mcpServerInfo.PromptCount == 0)
-        // {
-        //     mcpBuilder.WithListPromptsHandler((s, b) =>
-        //     {
-        //         return Task.FromResult(new ListPromptsResult());
-        //     });
-        //     mcpBuilder.WithGetPromptHandler( (s, b) =>
-        //     {
-        //         return Task.FromResult(new GetPromptResult());
-        //     });
-        // }
-        //
-        // if (mcpServerInfo.ResourceCount == 0)
-        // {
-        //     mcpBuilder.WithListResourcesHandler((a, b) => Task.FromResult(new ListResourcesResult()));
-        // }
-
-
         var logLevel = settings.EnableLogging == true ? LogLevel.Debug : LogLevel.None;
 
-        hostBuilder.Logging.SetMinimumLevel(logLevel).AddSpectreConsole(config =>
+        if (settings.HostProtocol?.ToLower() == "http")
         {
-            config.SetMinimumLevel(logLevel);
-        });
+            var hostBuilder = WebApplication.CreateBuilder();
+
+            var mcpBuilder = hostBuilder.Services
+                .AddMcpServer()
+                .WithHttpTransport()
+                .WithQuickMCP(mcpServerInfo);
+
+            hostBuilder.Logging.SetMinimumLevel(logLevel).AddSpectreConsole(config =>
+            {
+                config.SetMinimumLevel(logLevel);
+            });
 
 
-        if (settings.EnableLogging == true)
-            AnsiConsole.MarkupLine($"[bold]MCP server started...[/]");
-        //AnsiConsole.MarkupLine("[green]MCP Server Started (Press Ctrl+C to exit)[/]");
-        await hostBuilder.Build().RunAsync();
+            var app = hostBuilder.Build();
+            app.MapMcp();
+
+            if (settings.EnableLogging == true)
+            {
+                AnsiConsole.MarkupLine($"[bold]MCP server started...[/]");
+            }
+            await app.RunAsync();
+        }
+        else
+        {
+
+            if (settings.EnableLogging == true)
+                AnsiConsole.MarkupLine($"[bold]Using STDIO transport...[/]");
+
+            var hostBuilder = Host.CreateApplicationBuilder();
+
+            hostBuilder.Logging.SetMinimumLevel(logLevel).AddSpectreConsole(config =>
+            {
+                config.SetMinimumLevel(logLevel);
+            });
+
+            var app = hostBuilder.Build();
+
+            if (settings.EnableLogging == true)
+            {
+                AnsiConsole.MarkupLine($"[bold]MCP server started...[/]");
+            }
+
+            await app.RunAsync();
+        }
+
         return 0;
     }
 }
--- src/QuickMCP.CLI/Commands/Serve/ServerCommandSettings.cs
+++ src/QuickMCP.CLI/Commands/Serve/ServerCommandSettings.cs
@@ -37,6 +37,11 @@ public class ServerCommandSettings : CommandSettings
     [CommandOption("-l|--logging")]
     public bool? EnableLogging { get; set; }
 
+    [Description("Host Protocol.STDIO or HTTP.")]
+    [CommandOption("-h|--host-protocol")]
+    public string? HostProtocol { get; set; }
+
+
     public override ValidationResult Validate()
     {
         if (string.IsNullOrWhiteSpace(ConfigPath) && string.IsNullOrWhiteSpace(ConfigEnvVar) && string.IsNullOrEmpty(SpecPath) && string.IsNullOrEmpty(SpecUrl) && string.IsNullOrEmpty(ServerId))
