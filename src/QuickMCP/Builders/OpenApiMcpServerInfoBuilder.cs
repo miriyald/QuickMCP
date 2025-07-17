1be36d2a1722fa76af33e128022fadd66ad2854e
Dileep Miriyala (Moody's) <MiriyalD@moodys.com>
2025-07-17 13:07:18 -0400

    Revert Enum Changes
    Signed-off-by: Dileep Miriyala (Moody's) <MiriyalD@moodys.com>

--- src/QuickMCP/Builders/OpenApiMcpServerInfoBuilder.cs
+++ src/QuickMCP/Builders/OpenApiMcpServerInfoBuilder.cs
@@ -3,6 +3,7 @@ using QuickMCP.Abstractions;
 using QuickMCP.Helpers;
 using QuickMCP.Types;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Microsoft.OpenApi.Readers;
 using Newtonsoft.Json;
@@ -171,7 +172,11 @@ public class OpenApiMcpServerInfoBuilder : HttpMcpServerInfoBuilder
 #endif
 
             fileContent = ConvertToOpenApi30Json(filePath, fileContent);
-            var reader = new OpenApiStringReader();
+            var reader = new OpenApiStringReader(new OpenApiReaderSettings()
+            {
+
+            });
+
             var result = reader.Read(fileContent, out var diagnostic);
 
             if (diagnostic.Errors.Count > 0)
@@ -402,18 +407,8 @@ public class OpenApiMcpServerInfoBuilder : HttpMcpServerInfoBuilder
                 var op = operation.Value;
 
                 // Generate operation ID
-                var opId = op.OperationId;
-
-                if (string.IsNullOrEmpty(opId))
-                {
-                    // Get the Last segment of the path and sanitize it
-                    var pathSegments = path.Key.Split('/').Where(s => !string.IsNullOrEmpty(s)).ToArray();
-                    var lastSegment = pathSegments.Length > 0 ? pathSegments[pathSegments.Length - 1] : "";
-                    opId = $"{method}_{lastSegment}".Replace("/", "_").Replace("{", "").Replace("}", "");
-                }
-
-
-
+                var opId = op.OperationId ??
+                           $"{method}_{path.Key.Replace("/", "_").Replace("{", "").Replace("}", "")}";
                 var sanitizedOpId = StringHelpers.SanitizeToolName(opId);
 
                 // Extract parameters
@@ -583,7 +578,14 @@ public class OpenApiMcpServerInfoBuilder : HttpMcpServerInfoBuilder
             {
                 if (enumValue != null)
                 {
-                    enumArray.Add(enumValue.ToString());
+                    if (enumValue is OpenApiString stringValue)
+                    {
+                        enumArray.Add(stringValue.Value);
+                    }
+                    else
+                    {
+                        enumArray.Add(enumValue.ToString());
+                    }
                 }
             }
 
